0b5a2f0d93d8adfdbf43b751510981370f36a645
sverosak <sveinung.rosaker@finn.no>
2016-05-20 13:15:19 +0200

    move code from github/finn-no/spaden

---
+++ README.md
@@ -0,0 +1,14 @@
+# Spaden PostCSS config
+
+Dependencies and config for postcss usage inside spaden.
+
+https://github.com/finn-no/spaden
+
+
+# Usage
+
+```:javascript
+    const config = require('spaden-postcss-config');
+
+    postcss(config());
+```
---
+++ index.js
@@ -0,0 +1,41 @@
+'use strict';
+const path = require('path');
+let spadenPath;
+
+// support as a dependency inside or aside to spaden
+try {
+    spadenPath = path.dirname(
+        require.resolve('spaden/package.json')
+    )
+} catch(e) {
+    spadenPath = path.dirname(
+        path.resolve(__dirname, '..', '..', 'package.json')
+    );
+}
+
+// exports all plugins to run when building
+module.exports = function(config) {
+    const input = {
+        postcss: {
+            path: [
+                process.cwd(),
+                path.resolve(
+                    spadenPath, 'src', 'styles'
+                )
+            ]
+        }
+    };
+
+    if (config && config.postcss) {
+        // TODO when needed merge options
+    }
+
+    return [
+        require('postcss-import')(input.postcss),
+        require('postcss-custom-properties')(),
+        // require('postcss-nested'),
+        require('postcss-media-minmax')(),
+        require('postcss-custom-media')(),
+        require('autoprefixer')({ browsers: ['last 5 versions'] })
+    ];
+};
